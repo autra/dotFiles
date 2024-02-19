5518a05f22761f357d9baace47392a7b66c1ffcc
Augustin Trancart <augustin.trancart@gmail.com>
2024-02-19 08:16:48 +0100

    nix: first draft

---
+++ home.nix
@@ -0,0 +1,120 @@
+{ config, pkgs, ... }:
+
+{
+  # TODO only ok for non nixos 
+  targets.genericLinux.enable = true;
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    # prerequisite
+    fira-code-nerdfont 
+    # git utilities
+    git
+    git-extras
+    tig
+    glab
+    delta
+    # shell
+    bc # my tmux config depends on it
+    tmux
+    zsh
+    oh-my-zsh
+    fd
+    fzf
+    autojump
+    bat
+    jq
+    # powerline
+    powerline
+    powerline-symbols
+    # editors
+    lunarvim
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+
+    # SCMS
+    ".gitconfig".source = ~/dotFiles/.gitconfig;
+    ".git_commit_msg.txt".source = ~/dotFiles/.git_commit_msg.txt;
+    ".hgrc".source = ~/dotFiles/.hgrc;
+    # git scripts
+    "~/bin/git-sw".source = ~/dotFiles/git_scripts/git-sw;
+    "~/bin/git-delete-branches".source = ~/dotFiles/git_scripts/git-delete-branches;
+
+
+    # TODO link git_scripts to ~/bin
+
+    # shells
+    ".aliases".source = ~/dotFiles/.aliases;
+    ".bashrc".source = ~/dotFiles/.bashrc;
+    ".zshrc".source = ~/dotFiles/.zshrc;
+    "~/.oh-my-zsh/custom/themes/muse-mod.zsh-theme".source = ~/dotFiles/muse-mod.zsh-theme;
+    # TODO zsh plugins
+    # tmux
+    ".tmux.conf".source = ~/dotFiles/.tmux.conf;
+    # TODO clone tpm ?
+ 
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/augustin/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
