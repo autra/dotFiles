b743a58a415117f847ab11edbf825f603218ad3f
Augustin Trancart <augustin.trancart@gmail.com>
2024-02-24 14:24:42 +0100

    Nix: remove unused comment

--- nix/home.nix
+++ nix/home.nix
@@ -24,10 +24,6 @@ in {
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
