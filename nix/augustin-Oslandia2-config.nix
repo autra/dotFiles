4f0d7c4e182b9d4cc2434d5c3b09188b3e161baf
Augustin Trancart <augustin.trancart@oslandia.com>
2024-04-19 10:23:48 +0200

    nixos: add fingerprint support

--- nix/augustin-Oslandia2-config.nix
+++ nix/augustin-Oslandia2-config.nix
@@ -43,4 +43,5 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  services.fprintd.enable = true;
 }
