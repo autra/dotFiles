498d46ab44b360f8cf374605dc0dd870342650ed
Augustin Trancart <augustin.trancart@oslandia.com>
2024-06-01 14:21:51 +0200

    remove comment

--- nix/home_cli.nix
+++ nix/home_cli.nix
@@ -55,19 +55,6 @@ in rec {
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-    # prerequisite
     fira-code-nerdfont 
 
     # util
