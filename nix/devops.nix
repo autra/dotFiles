abe63f4182174332bfc5413c1e1b230d03681d08
Augustin Trancart <augustin.trancart@oslandia.com>
2024-06-01 14:20:27 +0200

    devops: vagrant and virtualbox

--- nix/devops.nix
+++ nix/devops.nix
@@ -1,16 +1,40 @@
 {config, pkgs, ...}:
 {
-  environment.systemPackages = with pkgs; [
-    virtualbox
+  config.environment.systemPackages = with pkgs; [
     lxc
     lxd
     vagrant
     ansible
-  ];
+
+    # for vagrant shared folders
+    # nfs-utils
+  ];  
+
+  # Minimal configuration for NFS support with Vagrant.
+  config.services.nfs.server.enable = true;
+  
+  # Add firewall exception for VirtualBox provider 
+  config.networking.firewall.extraCommands = ''
+    ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
+  '';
+
+  # Add firewall exception for libvirt provider when using NFSv4 
+  config.networking.firewall.interfaces."virbr1" = {                                   
+    allowedTCPPorts = [ 2049 ];                                               
+    allowedUDPPorts = [ 2049 ];                                               
+  };
+
+  # virtualbox
+  config.nixpkgs.config.allowUnfree = true;
+  config.virtualisation.virtualbox.host.enable = true;
+  config.users.extraGroups.vboxusers.members = [ config.mine.common.user ];
+  config.virtualisation.virtualbox.host.enableExtensionPack = true;
+  config.virtualisation.virtualbox.guest.enable = true;
+  config.virtualisation.virtualbox.guest.x11 = true;
 
   # docker
-  virtualisation.docker.enable = true;
-  users.users.${config.mine.common.user} = {
+  config.virtualisation.docker.enable = true;
+  config.users.users.${config.mine.common.user} = {
     extraGroups = [ "docker" ];
   };
 }
