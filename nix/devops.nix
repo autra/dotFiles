2837086bc99d92137516840c0cb3a4b20ab84421
Augustin Trancart <augustin.trancart@oslandia.com>
2024-08-17 23:08:19 +0200

    nix(devops): formatting

--- nix/devops.nix
+++ nix/devops.nix
@@ -1,27 +1,28 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
   config.environment.systemPackages = with pkgs; [
     lxc
     lxd-lts
     vagrant
     ansible
+    # libvirt
 
     # for vagrant shared folders
     # nfs-utils
-  ];  
+  ];
 
   # Minimal configuration for NFS support with Vagrant.
   config.services.nfs.server.enable = true;
-  
+
   # Add firewall exception for VirtualBox provider 
   config.networking.firewall.extraCommands = ''
     ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
   '';
 
   # Add firewall exception for libvirt provider when using NFSv4 
-  config.networking.firewall.interfaces."virbr1" = {                                   
-    allowedTCPPorts = [ 2049 ];                                               
-    allowedUDPPorts = [ 2049 ];                                               
+  config.networking.firewall.interfaces."virbr1" = {
+    allowedTCPPorts = [ 2049 ];
+    allowedUDPPorts = [ 2049 ];
   };
 
   # virtualbox
