2ce84a0dd59726b825b79eacc40ff6c544a78eae
Augustin Trancart <augustin.trancart@oslandia.com>
2024-12-15 21:36:19 +0100

    nix: formatting

--- nix/augustin-Oslandia2-config.nix
+++ nix/augustin-Oslandia2-config.nix
@@ -25,18 +25,19 @@
 
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/NIXROOT";
+    {
+      device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-label/NIXBOOT";
+    {
+      device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-label/swap"; }
-    ];
+    [{ device = "/dev/disk/by-label/swap"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
