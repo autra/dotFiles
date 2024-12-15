40fb6e08847983212dc599a375d7219a6a975667
Augustin Trancart <augustin.trancart@oslandia.com>
2024-12-15 21:00:37 +0100

    nix(stylix): switch to kantix colorscheme

--- nix/stylix_common.nix
+++ nix/stylix_common.nix
@@ -17,28 +17,31 @@
     };
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 # base16Scheme = "${pkgs.base16-schemes}/share/themes/github.yaml";
+    # https://coolors.co/005f87-2a9d8f-17b36f-8ab17d-e2b33c-ec831f-c21111-b31765
+    # ["005f87","2a9d8f","17b36f","8ab17d","e2b33c","ec831f","c21111","b31765"]
+    # chatgpt is able to convert that into konsole colorscheme with a bit of persuasion
     base16Scheme = {
       system = "base16";
       name = "Kantix  ";
       author = "autra";
       variant = "light";
       palette = {   
-        base00 = "ffffff"; # Background
-          base01 = "eaeaea"; # Lighter Background
-          base02 = "bcbcbc"; # Selection Background
-          base03 = "545454"; # Comments/Dimmed Foreground
-          base04 = "8c8c8c"; # Darker Foreground
-          base05 = "000000"; # Default Foreground
-          base06 = "000000"; # Brighter Foreground
-          base07 = "000000"; # Lightest Foreground
-          base08 = "b21818"; # Red/Error
-          base09 = "ec831f"; # Orange/Warning
-          base0A = "ec831f"; # Yellow/Highlight
-          base0B = "18b218"; # Green/Success
-          base0C = "159a9a"; # Cyan/Special
-          base0D = "005f87"; # Blue/Information
-          base0E = "b218b2"; # Purple/Keywords
-          base0F = "ec831f"; # Brown/Deprecated
+        base00 = "ffffff"; # #ffffff # Background
+        base01 = "eaeaea"; # #eaeaea # Lighter Background
+        base02 = "bcbcbc"; # #bcbcbc # Selection Background
+        base03 = "797979"; # #545454 # Comments/Dimmed Foreground
+        base04 = "000000"; # #000000 # Darker Foreground
+        base05 = "181818"; # #181818 # Default Foreground
+        base06 = "4b4b4b"; # #4b4b4b # Brighter Foreground
+        base07 = "8c8c8c"; # #8c8c8c # Lightest Foreground
+        base08 = "c21111"; # #C21111 #C3123E #b22d3a # Red/Error
+        base09 = "d5a804"; # #d5a804 # Orange/Warning
+        base0A = "ec831f"; # #EC831F #ec831f # Yellow/Highlight
+        base0B = "17b36f"; # #17B36F #8AB17D #18b249 # Green/Success
+        base0C = "2A9D8F"; # #2a9d8f #159a9a # Cyan/Special
+        base0D = "005F87"; # #264653 #005f87 # Blue/Information
+        base0E = "B31765"; # #b31765 #b218b2 # Purple/Keywords
+        base0F = "aa5e16"; # #e2b33c #aa5e16 # Brown/Deprecated
       };
     };
     cursor = {
