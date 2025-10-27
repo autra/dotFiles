671af4f9a36f608dc797e914effd0fcf368a245f
Augustin Trancart <augustin.trancart@gmail.com>
2025-10-27 22:15:58 +0100

    wip nix-on-droid

---
+++ nix/nix-on-droid/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Basic example of Nix-on-Droid system config.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-on-droid }: {
+
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      modules = [ ./nix-on-droid.nix ];
+    };
+
+  };
+}
