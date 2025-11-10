d05aa85edaf9464f224fc2582368f32fac6cedee
Augustin Trancart <augustin.trancart@oslandia.com>
2025-11-10 22:09:02 +0100

    nix: install nix-update

--- nix/home-manager/nix_niceties.nix
+++ nix/home-manager/nix_niceties.nix
@@ -3,6 +3,10 @@
 {
   home.packages = with pkgs; [
     nix-inspect
+    nix-update
+    # nix-search-tv?
+    # https://discourse.nixos.org/t/npc-cli-to-view-and-bisect-nixpkgs-channel-history/71242
+# https://github.com/blitz/hydrasect
   ];
   
 }
