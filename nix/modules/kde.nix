d7bfd8f6f92466c6aba6558226a2230ded8b9d05
Augustin Trancart <augustin.trancart@oslandia.com>
2025-11-23 14:14:00 +0100

    nix: remove xserver, not supported any more in nixpkgs

--- nix/modules/kde.nix
+++ nix/modules/kde.nix
@@ -2,7 +2,7 @@
 
 {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
