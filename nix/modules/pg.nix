d189621f6343dc80d9b761656533fb825d520311
Augustin Trancart <augustin.trancart@oslandia.com>
2025-12-01 15:52:49 +0100

    nix(pg): add upgrade script like documented

--- nix/modules/pg.nix
+++ nix/modules/pg.nix
@@ -1,15 +1,50 @@
-{ pkgs, config,...}:
+{ pkgs, config, ... }:
+let
+  upgrade_script = (
+    let
+      # XXX specify the postgresql package you'd like to upgrade to.
+      # Do not forget to list the extensions you need.
+      newPostgres = pkgs.postgresql.withPackages (pp: [
+        pp.postgis
+      ]);
+      cfg = config.services.postgresql;
+    in
+    pkgs.writeScriptBin "upgrade-pg-cluster" ''
+      set -eux
+      # XXX it's perhaps advisable to stop all services that depend on postgresql
+      systemctl stop postgresql
+
+      export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+      export NEWBIN="${newPostgres}/bin"
+
+      export OLDDATA="${cfg.dataDir}"
+      export OLDBIN="${cfg.finalPackage}/bin"
+
+      install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+      cd "$NEWDATA"
+      sudo -u postgres "$NEWBIN/initdb" -D "$NEWDATA" ${lib.escapeShellArgs cfg.initdbArgs}
+
+      sudo -u postgres "$NEWBIN/pg_upgrade" \
+        --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+        --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
+        "$@"
+    ''
+  );
+in
 {
   config.environment.systemPackages = with pkgs; [
+    upgrade_script
     pgadmin4-desktopmode
     dbeaver-bin
     rainfrog
   ];
   config.services.postgresql = {
     enable = true;
+    # this is necessary, because otherwise postgresql stays in the version at the time of system.stateVersion
+    package = pkgs.postgresql_17;
     extensions = (ps: with ps; [ postgis ]);
     ensureDatabases = [ config.mine.common.user ];
-    ensureUsers = [{ 
+    ensureUsers = [{
       name = config.mine.common.user;
       ensureDBOwnership = true;
       ensureClauses.superuser = true;
