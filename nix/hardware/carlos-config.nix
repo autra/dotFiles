afc4b4cfec6f68ef9aedd6d2756cb0c9d0d6e6eb
Augustin Trancart <augustin.trancart@oslandia.com>
2026-01-04 22:04:16 +0100

    nix: activate wol on carlos

--- nix/hardware/carlos-config.nix
+++ nix/hardware/carlos-config.nix
@@ -29,14 +29,17 @@
     [ { device = "/dev/disk/by-label/swap"; }
     ];
 
-  networking.hostName = "carlos"; # Define your hostname.
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  networking = {
+    hostName = "carlos"; 
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+    useDHCP = lib.mkDefault true;
+    interfaces.enp4s0.wakeOnLan.enable = true;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
