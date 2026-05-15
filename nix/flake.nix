1baed5c1c2f87bd3e3dc5678239cd3d3404cd623
Augustin Trancart <augustin.trancart@oslandia.com>
2026-05-15 21:26:11 +0200

    nix: switch to NixOS 26.05 🎉

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     self.submodules = true;
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:nix-community/stylix/release-25.11";
+      url = "github:nix-community/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     osladoc = {
