65ab78464cce9b06cf56b9f59ea24088c29aaefe
Your Name <you@example.com>
2026-06-06 13:27:17 +0000

    nix-on-droid 1st iteration

---
+++ nix-on-droid/nix-on-droid.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    su
+    # User-facing stuff that you really really want to have
+    neovim # or some other editor, e.g. nano or neovim
+
+    # Some common stuff that people expect to have
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    #gnupg
+    #gnused
+    git
+    gnutar
+    bzip2
+    gzip
+    #xz
+    zip
+    unzip
+	
+    openssh
+    inetutils
+    curl
+    starship
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+}
